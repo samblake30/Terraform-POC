b8dd6ca0e31df95a8bd52e81f5292ee42f5a5474
Ankit Sambhare <ankitsambhare3004@gmail.com>
2020-09-06 16:29:24 +0530

    Updating the name of the droplet and resource name

--- droplet.tf
+++ droplet.tf
@@ -3,9 +3,9 @@ provider "digitalocean" {
   token = "95b276cd47516648ec1c5f543f7f33a01e6c7a4d865ed157ce9f49ce0181c173"
 }
 
-resource "digitalocean_droplet" "kplabsdroplet" {
+resource "digitalocean_droplet" "mydrop" {
   image  = "ubuntu-18-04-x64"
-  name   = "web-1"
+  name   = "samblake30"
   region = "nyc1"
   size   = "s-1vcpu-1gb"
 }
