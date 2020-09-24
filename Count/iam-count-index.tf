ea928165d6c7c437ef5a99594308ebed3be80b42
Ankit Sambhare <ankitsambhare3004@gmail.com>
2020-09-24 11:00:12 +0530

    Update of all related Terraform Use Case

--- Count/iam-count-index.tf
+++ Count/iam-count-index.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAIILHES55MS5MGD5Q"
-  secret_key = "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"
-}
 
 #This will attach the iterated number along the count parameter
 resource "aws_iam_user" "myiam" {
